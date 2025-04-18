4df6f665dc8fc6ff07b90ed8387e1b84e1b9abf0
vitorjsss <vitorjsssmamede.1@gmail.com>
2025-04-18 16:57:24 -0300

    modified code

--- tabuleiro.cpp
+++ tabuleiro.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <tuple>
-#include <random>
+#include <utility>
+#include <cstdlib>
 #include <ctime>
 
 using namespace std;
@@ -29,11 +29,11 @@ pair<vector<vector<char>>, vector<pair<int, int>>> gerar_tabuleiro_com_bloqueios
     vector<vector<char>> tabuleiro(n, vector<char>(n, '.'));
     set<pair<int, int>> bloqueios;
 
-    while (bloqueios.size() < num_bloqueios)
+    while ((int)bloqueios.size() < num_bloqueios)
     {
         int i = rand() % n;
         int j = rand() % n;
-        pair<int, int> pos = {i, j};
+        pair<int, int> pos = make_pair(i, j);
         if (bloqueios.find(pos) == bloqueios.end())
         {
             tabuleiro[i][j] = 'X';
@@ -42,7 +42,7 @@ pair<vector<vector<char>>, vector<pair<int, int>>> gerar_tabuleiro_com_bloqueios
     }
 
     vector<pair<int, int>> bloqueios_lista(bloqueios.begin(), bloqueios.end());
-    return {tabuleiro, bloqueios_lista};
+    return make_pair(tabuleiro, bloqueios_lista);
 }
 
 void imprimir_tabuleiro(const vector<vector<char>> &tabuleiro, int limite = 32)
@@ -50,7 +50,7 @@ void imprimir_tabuleiro(const vector<vector<char>> &tabuleiro, int limite = 32)
     int n = tabuleiro.size();
     for (int i = 0; i < min(n, limite); ++i)
     {
-        for (int j = 0; j < min(int(tabuleiro[i].size()), limite); ++j)
+        for (int j = 0; j < min((int)tabuleiro[i].size(), limite); ++j)
         {
             cout << tabuleiro[i][j] << ' ';
         }
@@ -61,13 +61,16 @@ void imprimir_tabuleiro(const vector<vector<char>> &tabuleiro, int limite = 32)
 int main()
 {
     int n = 8;
-    auto [tabuleiro, bloqueios] = gerar_tabuleiro_com_bloqueios_melhorado(n, -1, 10);
+    pair<vector<vector<char>>, vector<pair<int, int>>> resultado = gerar_tabuleiro_com_bloqueios_melhorado(n, -1, 10);
+    vector<vector<char>> tabuleiro = resultado.first;
+    vector<pair<int, int>> bloqueios = resultado.second;
+
     imprimir_tabuleiro(tabuleiro);
 
     cout << "\nBloqueios:\n";
-    for (const auto &[i, j] : bloqueios)
+    for (size_t k = 0; k < bloqueios.size(); ++k)
     {
-        cout << "(" << i << ", " << j << ") ";
+        cout << "(" << bloqueios[k].first << ", " << bloqueios[k].second << ") ";
     }
     cout << endl;
 
